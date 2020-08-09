04fd47e258d944d1653f7d152bceef4dda73f3a3
caesiumy <mn0658@naver.com>
2020-08-09 18:51:30 +0900

    feat: Update pybo urls.py

--- pybo/urls.py
+++ pybo/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+app_name = 'pybo'
 
 urlpatterns = [
     path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
