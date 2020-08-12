0291581dee6f4e79e58b50a98ede5de7a2a94fc1
caesiumy <mn0658@naver.com>
2020-08-12 18:46:49 +0900

    feat: Update forms without as_p

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Question
+from .models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -7,12 +7,21 @@ class QuestionForm(forms.ModelForm):
         fields = ['subject', 'content']
 
         # form을 위젯으로 만드는 방법
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),            
-        }
+        # widgets = {
+        #     'subject': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),            
+        # }
 
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+
+        labels = {
+            'content': '답변 내용'
         }
