6493f2566f590168aaccf9f745225f0e0d2df98c
caesiumy <mn0658@naver.com>
2020-08-17 00:37:23 +0900

    feat: Add modify question

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 
@@ -68,6 +69,32 @@ def question_create(request):
             return redirect('pybo:index')
     else:
         form = QuestionForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question_id)
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)
+
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = QuestionForm(instance=question)
 
     context = {
         'form': form
