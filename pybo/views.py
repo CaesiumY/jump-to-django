cd40c7eb8e4a1c0bb2da50e5633970dd9428cc3c
caesiumy <mn0658@naver.com>
2020-08-18 23:49:53 +0900

    feat: Add modify answer

--- pybo/views.py
+++ pybo/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Question
+from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 
 # Create your views here.
@@ -114,3 +114,28 @@ def question_delete(request, question_id):
     question.delete()
 
     return redirect('pybo:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id = answer.question.id)
+    
+    if(request.method == 'POST'):
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id = answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+
+    context = {
+        'answer': answer,
+        'form': form
+    }
+
+    return render(request, 'pybo/answer_form.html', context)
