e967d287caa29fe6e10104f96b70cd990371aee9
caesiumy <mn0658@naver.com>
2020-08-22 18:47:04 +0900

    feat: Add vote answer

--- pybo/views/vote_views.py
+++ pybo/views/vote_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from ..models import Question
+from ..models import Question, Answer
 
 @login_required(login_url='common:login')
 def vote_question(request, question_id):
@@ -14,3 +14,15 @@ def vote_question(request, question_id):
         question.voter.add(request.user)
 
     return redirect('pybo:detail', question_id=question_id)
+
+
+@login_required(login_url='common:login')
+def vote_answer(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user == answer.author:
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+
+    return redirect('pybo:detail', question_id=answer.question.id)
