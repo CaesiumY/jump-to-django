f8fa952a35522a6f5421e65ec23a4fa8fb166feb
caesiumy <mn0658@naver.com>
2020-08-27 21:59:44 +0900

    feat: Add sort feature

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, Count
 from ..models import Question
 
 def index(request):
     page = request.GET.get('page', '1')
     kw = request.GET.get('kw', '')
+    so = request.GET.get('so', 'recent')
 
-    question_list = Question.objects.order_by('-create_date')
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:
+        question_list = Question.objects.order_by('-create_date')
 
     if kw:
         question_list = question_list.filter(
@@ -23,7 +29,8 @@ def index(request):
     context = {
         'question_list': page_obj,
         'page': page,
-        'kw': kw
+        'kw': kw,
+        'so': so
     }
 
     return render(request, 'pybo/question_list.html', context)
