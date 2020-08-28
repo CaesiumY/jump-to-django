9ea3b945becd1464e00fd9b93c49fc2aca851766
caesiumy <mn0658@naver.com>
2020-08-28 22:50:52 +0900

    feat: Add 404 error page

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,8 @@
 from .base import *
 
 # Put your IP Address
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = ['*'] 
+STATIC_ROOT = os.path.join(BASE_DIR, 'pybo/static/')
+STATICFILES_DIRS = []
+
+DEBUG = False
